27093b5cd87b83b489195b060ed5e5611435999b
Cassio Rodrigo Peluso <cpeluso@Cassios-MacBook-Air.local>
2021-07-31 18:32:02 -0300

    added requirments file

--- modelo2.py
+++ modelo2.py
@@ -1,5 +1,24 @@
 
 
 class Cashier:
+    stock = ["Soup", "Bread", "Milk", "Apples"]
+    prices = [0.65, 0.80, 1.30, 1.0]
+    product_price = dict(zip(stock,prices))
+    basket = []
     def __init__(self, string_items):
         self.items_list = string_items.split(" ")
+        for product in self.items_list:
+            if product in self.stock:
+                self.basket.append(product)
+
+    def subtotal_calc(self):
+        subtotal = float()
+        for product in self.basket:
+            subtotal += self.product_price[product]
+        return subtotal
+
+
+if __name__ == "__main__":
+    new_cashier = Cashier("PriceBasket Apples Milk Bread")
+    print(new_cashier.basket)
+    print(new_cashier.subtotal_calc())
--- teste_modelo2.py
+++ teste_modelo2.py
@@ -1,5 +1,30 @@
 import unittest
+import modelo2
+
 
 class TestModel2(unittest.TestCase):
-    def test_init(self):
-        self.assertIs()
+
+    def setUp(self):
+        stock = ["Soup", "Bread", "Milk", "Apples"]
+        cart1 = "Apples Milk Bread"
+        cart2 = "PriceBasket Apples Milk Bread"
+        self.new_cashier = modelo2.Cashier(string_items=cart2)
+
+    def tearDown(self):
+        pass
+
+    def test_initial(self):
+        self.assertIs(type(self.new_cashier.items_list), list)
+        self.assertIs(type(self.new_cashier.product_price), dict)
+        self.assertIn("Soup", self.new_cashier.product_price)
+        self.assertIn("Milk", self.new_cashier.product_price)
+        self.assertNotIn("PriceBasket", self.new_cashier.basket)
+        self.assertEqual(3, len(self.new_cashier.basket))
+
+    def test_subtotal(self):
+        TestModel2().assertAlmostEqual(3.1, self.new_cashier.subtotal_calc(), 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
