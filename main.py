20b47bc2894fe00f8d605023f4bf75aaac4f6d1e
Cassio Rodrigo Peluso <cpeluso@Cassios-MacBook-Air.local>
2021-08-03 11:15:00 -0300

    renamed files and generate new requirements.txt file

--- main.py
+++ main.py
@@ -1,156 +1,170 @@
-import locale
-import datetime
-from locale import LC_ALL, setlocale
-
+from locale import LC_ALL, setlocale, currency
+import re
+from datetime import date
 setlocale(LC_ALL, "en_ie")
 
 
 class Cashier:
     """
-    creates a class that will create an cashier object 
-    to manage items in the basket, prices and promotions
-    On the object creation, user might want to pass the week number
+    Creates an cashier object. It requires either a list on the object creation
+    or a string containing the name of the products passed through
+    Cashier.from_string(<string in here>).
+
+    :example:
+    Usage examples passing a string:
+    new_cashier = Cashier.from_string("PriceBasket Apples Milk bread Soupsoup", year_week=30)
+    print(new_cashier)
+
+    or a list:
+
+    new_cashier1 = Cashier(["PriceBasket", "Apples", "Milk", "Bread"])
+        <obs.: item 'PriceBasket' will be ignored.>
+    print(new_cashier1)
+
+    __str__ method was rewritten to print the receipt itself.
+    __repr__ method as standards
     """
+    stock = ["Soup", "Bread", "Milk", "Apples"]
+    prices = [0.65, 0.80, 1.30, 1.0]
+    product_price = dict(zip(stock, prices))
 
-    def __init__(self, **kwargs):
+    def __init__(self, items_list, year_week=None):
+        self.items_string = " "
+        self.basket = []
+        self.items_list = items_list
+        if year_week:
+            if type(year_week) not in {int, str}:
+                raise TypeError("year_week refers to the week number of the year"
+                                "and should be an integer")
+            self.year_week = int(year_week)
+        for product in items_list:
+            if product in self.stock:
+                self.basket.append(product)
+        self.items_string = self.items_string.join(str(e) for e in self.basket)
 
-        if kwargs.get("week_number"):
-            apple_week = kwargs.get("week_number")
+    def __repr__(self):
+        return f"Cashier({self.items_list})"
+
+    def __str__(self):
+        total, discount_apples, discount_bread = self.apply_promotion()
+        items_descriptions = "\n\t".join(self.basket)
+
+        if discount_apples > 0:
+            string_discount_apples = f"Apples: -{currency(discount_apples)}\n"
         else:
-            apple_week = datetime.date.today().isocalendar()[1]
+            string_discount_apples = ""
 
-        if apple_week == datetime.date.today().isocalendar()[1]:
-            self.apple_offer = True
+        if discount_bread > 0:
+            string_discount_bread = f"Bread: -{currency(discount_bread)}"
         else:
-            self.apple_offer = False
+            string_discount_bread = ""
 
-    basket_items = []
-    basket_prices = []
-    total = float()
-    price = [{"index": 1, "product": "Soup", "price": 0.65}, {"index": 2, "product": "Bread", "price": 0.80},
-             {"index": 3, "product": "Milk", "price": 1.30}, {"index": 4, "product": "Apple", "price": 1.00}]
+        if discount_apples > 0 or discount_bread > 0:
+            offers_active = f'''
+    Offers active:
+    \t{string_discount_apples} {string_discount_bread}
+    '''
+        else:
+            offers_active = "(No offers avilable)"
+        descriptive = f'''
+        
+Products in the basket: {self.items_string}
+Subtotal: {currency(self.subtotal_calc())}
+{offers_active}
+Total: {currency(total)}
+Items:
+\t{items_descriptions}
+'''
+        return descriptive
 
-    def new_cart(self):
-        """
-        create a new cart in the comand line via numeric options menu.
-        number 0 finishes the insertion of items and put it forward to calculations
-        :return: a list of items in the cart
-        """
-        text = str()
-        for indexed_item in self.price:
-            text += f"\n{indexed_item['index']}) {indexed_item['product']}"
+    @classmethod
+    def from_string(cls, string_items, year_week=None):
+        if year_week is not None:
+            if type(string_items) != str:
+                raise TypeError(f"Type '{type(string_items).__name__}' not supported.\n"
+                                f"Please, pass a string to this classmethod")
 
-        while True:
-            try:
-                item = int(input(f"""
-                Which item would you like to add to chart?
-                {text}
-                Press 0 to when done adding items
-                """))
-                if item == 0:
-                    break
-                if item > 4:
-                    print("Please, choose a number in the list")
-                    # self.new_cart()
-                for product in self.price:
-                    if product["index"] == item:
-                        self.basket_items.append(product["product"])
-                        self.basket_prices.append(product["price"])
-            except ValueError:
-                print("Please, choose a *number* in the list")
+            if type(year_week) == str:
+                try:
+                    year_week = int(year_week)
+                except ValueError as verr:
+                    raise ValueError("Week must be a positive integer or a castable string")
+            if type(year_week) != int and year_week < 0:
+                raise ValueError("Week must be a positive integer")
+        list_items = []
+        for e in cls.stock:
+            matches = re.finditer(e, string_items, re.IGNORECASE)
+            for match in matches:
+                try:
+                    list_items.append(string_items[match.span()[0]:match.span()[1]].title())
+                except Exception as err:
+                    print(err)
+        return cls(list_items, year_week)
 
     def subtotal_calc(self):
         """
         Calculates the partial (subtotal) before conditions for promotions are checked
         and prices discounted
-        :return: subtotal to be used to calculate the total if discounts are applicable
-        """
-        subtotal = float(0)
-        for item_data in self.price:
-            if item_data["product"] in self.basket_items:
-                subtotal += item_data["price"] * self.basket_items.count(item_data["product"])
 
+        :return:Float result of the sum of products in the cart before discounts.
+        """
+
+        subtotal = 0.0
+        for product in self.basket:
+            subtotal += self.product_price[product]
         return subtotal
 
-    def check_promo_conditions(self, items_in_promo=None):
+    def valid_promotion(self):
         """
-        :param: list of items that will enable discounts this is acting as a place holder for next week if
-        merchant wants to apply same discount principles for other items eg.: Milk
-        This list should always have a len of 2.
-        Soup – €0.65 per tin
-        Bread – €0.80 per loaf
-        Milk – €1.30 per bottle
-        Apples – €1.00 per bag Current special offers are:
-        Apples have 10% off their normal price this week
-        Buy 2 tins of soup and get a loaf of bread for half price
-
-        this method will generate a string to fill the receipt and compute discounts (if any) applicable
-        to the current basket
-
-        :return: text of discounted products and discount values for bread and apples
+        Since apples offer depends on the week number, it assumes the offer is no long valid if no year_week number
+        is passed.
+        :return: Tuple with 2 Boolean values being the first True for apples offer active
+        and the second True for soup offer active. If either is not active, the returned value will be False
         """
-        # For future implementation of promotions
-        # Pass a list of prducts to receive discounts
-        text_bread = ""
-        discount_for_breads = 0
-        text_apple = ""
-        discount_apples = 0
+        this_week = date.today().isocalendar()[1]
+        offer_apples = False
+        offer_soup = False
+        try:
+            if self.year_week == this_week:
+                offer_apples = True
+        except AttributeError as aterror:
+            print(aterror)
+            print('As no week number was passed, Apples offer will be turned off')
 
-        if items_in_promo is None:
-            items_in_promo = ["Apple", "Soup"]
-        if self.apple_offer:
-            discount_for_apples = '10%'
-        else:
-            discount_for_apples = ''
+        if self.basket.count("Soup") >= 2:
+            offer_soup = True
+        return offer_apples, offer_soup
 
-        if items_in_promo[0] in self.basket_items:
-            if self.apple_offer:
-                discount_apples = self.basket_items.count(items_in_promo[0]) * 0.1
-                text_apple = f"{items_in_promo[0]} {discount_for_apples} off: -{locale.currency(discount_apples)}"
-        else:
-            text_apple = ""
-            discount_apples = 0
-        if self.basket_items.count(items_in_promo[1]) >= 2:
-            if "Bread" in self.basket_items:
-                for each in self.price:
-                    if each["product"] == "Bread":
-                        discount_for_breads = each["price"]/2
-                        text_bread = f"A loaf of bread: -{locale.currency(discount_for_breads)}"
-
-        text_receipt = f"{text_apple}\n{text_bread}"
-
-        if text_apple == "" and text_bread == "":
-            text_receipt = "no offers available"
-
-        return text_receipt, discount_for_breads, discount_apples
-
-    def sum_it_up(self):
+    def apply_promotion(self, apple_discount_rate=0.1):
         """
-        This will calculate the prices for the total, subtracting the discounted products.
-        It helps generating part of the receipt to be passed to the receipt generator method (create_receipts)
-        :return: the result of subtotal -
+        This method takes no arguments and apply offers discounts on the subtotal.
+        :return: a total float with applied discounts if any
         """
+        discount_apples = 0.0
+        discount_bread = 0.0
+        offer_apples, offer_soup = self.valid_promotion()
+        if offer_apples:
+            discount_apples = self.basket.count("Apples")*(apple_discount_rate*self.product_price["Apples"])
+        if offer_soup:
+            soup_discounts_possible = self.basket.count("Soup")//2
+            number_of_breadloaf = self.basket.count("Bread")
+            n_discounted_bread = 0
+            for n in range(soup_discounts_possible):
+                if n <= number_of_breadloaf:
+                    n_discounted_bread += 1
+            discount_bread = n_discounted_bread * (self.product_price["Bread"]/2)
 
-        subtotal = self.subtotal_calc()
-        text_offers, discount_breads, discount_apples = self.check_promo_conditions()
-
-        total = subtotal - discount_breads - discount_apples
-
-        return text_offers, total
-
-    def create_receipt(self):
-
-        full_receipt, total = self.sum_it_up()
-        print(f"Subtotal: {locale.currency(self.subtotal_calc())}")
-        print(full_receipt, "Total: ", locale.currency(total))
-        full_receipt = "\n".join(self.basket_items)
-
-        return full_receipt
+        total = self.subtotal_calc() - discount_apples - discount_bread
+        return total, discount_apples, discount_bread
 
 
 if __name__ == "__main__":
-    new_cashier = Cashier()
-    produtos = new_cashier.new_cart()
-    receipt = new_cashier.create_receipt()
-    print(receipt)
+    new_shopcart = str(input("Which items do you want to buy today?\n"))
+    new_cashier = Cashier.from_string(new_shopcart)
+    print(new_cashier)
+    """
+    to use with a passed python list, please, use te template bellow:
+    new_cashier = Cashier(["PriceBasket", "Apples", "Milk", "Bread"])
+    """
+
 
